e619ea3dca95a62fe529b93feca514870b680cce
Gonza-wf <121890733+Gonza-wf@users.noreply.github.com>
2026-01-02 22:28:50 -0300

    Update service worker to cache index.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'color-lab-v1';
-const assets = ['./colores.html', 'https://cdn.tailwindcss.com', 'https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.6.0/tinycolor.min.js'];
+xconst CACHE_NAME = 'color-lab-v1';
+const assets = ['./index.html', 'https://cdn.tailwindcss.com', 'https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.6.0/tinycolor.min.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
@@ -7,4 +7,5 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
 });
